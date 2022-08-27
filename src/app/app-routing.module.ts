84b19961b7ac1796e334967457aca35204f0b180
Em <geekfanboy@gmail.com>
2022-08-27 22:58:35 +0200

    Initial Commit. Alpha Version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignupSuccessComponent } from './components/signup-success/signup-success.component';
+import { SignupFailComponent } from './components/signup-fail/signup-fail.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SignupComponent },
+  { path: 'registered', component: SignupSuccessComponent},
+  { path: 'registerfail', component: SignupFailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+
+import { SignupComponent } from './components/signup/signup.component';
+import { SignupSuccessComponent } from './components/signup-success/signup-success.component';
+import { TrimInputDirective } from './directives/trim-input.directive';
+import { signupReducer } from './store/signup/signup.reducers';
+import { SignUpEffects } from './store/signup/signup.effects';
+import { SignupFailComponent } from './components/signup-fail/signup-fail.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SignupSuccessComponent,
+    TrimInputDirective,
+    SignupFailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,    
+    StoreModule.forRoot({signup: signupReducer}),
+    EffectsModule.forRoot([SignUpEffects])
   ],
   providers: [],
   bootstrap: [AppComponent]
